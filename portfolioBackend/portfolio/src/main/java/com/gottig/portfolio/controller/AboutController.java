90b107f5a54178fe537f3be32467f9c04e9ca4c7
gottigjavier <gottigjavier@gmail.com>
2022-04-21 10:58:36 -0300

    add global .gitignore

--- portfolioBackend/portfolio/src/main/java/com/gottig/portfolio/controller/AboutController.java
+++ portfolioBackend/portfolio/src/main/java/com/gottig/portfolio/controller/AboutController.java
@@ -2,18 +2,13 @@ package com.gottig.portfolio.controller;
 
 import com.gottig.portfolio.model.About;
 import com.gottig.portfolio.service.about.IAboutService;
-import static java.lang.Integer.parseInt;
-import static java.lang.Long.parseLong;
-import java.util.Arrays;
 import java.util.List;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
